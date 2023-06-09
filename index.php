b5498a27ea30053028ed9f4c4c94eabba7a4e7be
ehmenzala <ehmenzala@gmail.com>
2023-06-09 08:56:46 -0500

    Add rudimentary routing & update hrefs

--- index.php
+++ index.php
@@ -2,4 +2,17 @@
 
 session_start();
 
-require "views/index.html";
+$routes = [
+  '/biblioweb/' => 'views/index.html',
+  '/biblioweb/libros/' => 'views/libros.html',
+  '/biblioweb/autores/' => 'views/autores.html',
+  '/biblioweb/rating/' => 'views/rating.html',
+  '/biblioweb/admin/' => 'views/admin-view.html',
+];
+
+if (array_key_exists($_SERVER['REQUEST_URI'], $routes)) {
+  require $routes[$_SERVER['REQUEST_URI']];
+} else {
+  http_response_code(404);
+  echo 'NO SE ENCUENTRAAA';
+}
