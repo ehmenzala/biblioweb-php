30190008722499e5d5415ce669d053dad1ed4499
ehmenzala <ehmenzala@gmail.com>
2023-06-09 08:34:19 -0500

    Fix HTML semantics and add dialog cta button behavior

--- js/modals.js
+++ js/modals.js
@@ -7,8 +7,23 @@ const $openLogin = document.getElementById("open-login-dialog");
 const $closeRegister = document.getElementById("close-register-dialog");
 const $closeLogin = document.getElementById("close-login-dialog");
 
+const $closeAndOpenRegister = document.getElementById(
+  "close-and-open-register"
+);
+const $closeAndOpenLogin = document.getElementById("close-and-open-login");
+
 $openRegister.addEventListener("click", () => $registerDialog.showModal());
 $openLogin.addEventListener("click", () => $loginDialog.showModal());
 
 $closeRegister.addEventListener("click", () => $registerDialog.close());
 $closeLogin.addEventListener("click", () => $loginDialog.close());
+
+$closeAndOpenRegister.addEventListener("click", () => {
+  $loginDialog.close();
+  $registerDialog.showModal();
+});
+
+$closeAndOpenLogin.addEventListener("click", () => {
+  $registerDialog.close();
+  $loginDialog.showModal();
+});
