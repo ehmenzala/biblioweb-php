0e689c33dcaeece78df46b30dafaa77064d18a0e
ehmenzala <ehmenzala@gmail.com>
2023-06-10 22:42:26 -0500

    Update file structure - MVC arch pattern

--- index.php
+++ index.php
@@ -2,16 +2,16 @@
 
 session_start();
 
-require 'router/Router.php';
+require 'Router.php';
 
 $router = new Router();
 $uri = $_SERVER['REQUEST_URI'];
 $method = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
 
-$router->get('/biblioweb/', 'views/index.html');
-$router->get('/biblioweb/libros/', 'views/libros.html');
-$router->get('/biblioweb/autores/', 'views/autores.html');
-$router->get('/biblioweb/rating/', 'views/rating.html');
-$router->get('/biblioweb/admin/', 'views/admin-view.html');
+$router->get('/biblioweb/', 'src/views/index.html');
+$router->get('/biblioweb/libros/', 'src/views/libros.html');
+$router->get('/biblioweb/autores/', 'src/views/autores.html');
+$router->get('/biblioweb/rating/', 'src/views/rating.html');
+$router->get('/biblioweb/admin/', 'src/views/admin-view.html');
 
 $router->route($uri, $method);
