1c8bbd36ea1ec1e7a191149ab323aee7d3553ec7
ehmenzala <ehmenzala@gmail.com>
2023-06-13 13:52:44 -0500

    Update db script

--- db/biblioweb-database-script.sql
+++ db/biblioweb-database-script.sql
@@ -1,8 +1,7 @@
 CREATE TABLE autor
 (
-	id_autor             INTEGER NOT NULL,
+	id_autor             INTEGER AUTO_INCREMENT,
 	nombre_completo      VARCHAR(20) NOT NULL,
-	edad                 INTEGER NOT NULL,
 	foto_perfil          VARCHAR(75) NOT NULL,
 	PRIMARY KEY (id_autor)
 );
@@ -12,11 +11,23 @@ CREATE UNIQUE INDEX XPKautor ON autor
 	id_autor
 );
 
+CREATE TABLE genero
+(
+	id_genero            INTEGER AUTO_INCREMENT,
+	nombre               VARCHAR(30) NOT NULL,
+	PRIMARY KEY (id_genero)
+);
+
+CREATE UNIQUE INDEX XPKgenero ON genero
+(
+	id_genero
+);
+
 CREATE TABLE libro
 (
-	id_libro             INTEGER NOT NULL,
+	id_libro             INTEGER AUTO_INCREMENT,
+	id_genero            INTEGER NOT NULL,
 	titulo               VARCHAR(50) NOT NULL,
-	genero               VARCHAR(30) NOT NULL,
 	anio_publicacion     INTEGER NOT NULL,
 	num_paginas          INTEGER NOT NULL,
 	idioma               VARCHAR(20) NOT NULL,
@@ -24,30 +35,38 @@ CREATE TABLE libro
 	descripcion          VARCHAR(550) NOT NULL,
 	rating               INTEGER NOT NULL,
 	fragmento            VARCHAR(350) NOT NULL,
-	fecha_creacion       DATETIME NOT NULL,
-	PRIMARY KEY (id_libro)
+  portada              VARCHAR(120) NOT NULL,
+	fecha_creacion       DATETIME NOT NULL DEFAULT NOW(),
+	PRIMARY KEY (id_libro,id_genero),
+	FOREIGN KEY R_4 (id_genero) REFERENCES genero (id_genero)
 );
 
 CREATE UNIQUE INDEX XPKlibro ON libro
 (
-	id_libro
+	id_libro,
+	id_genero
 );
 
-
+CREATE INDEX XIF1libro ON libro
+(
+	id_genero
+);
 
 CREATE TABLE autor_libro
 (
 	id_autor             INTEGER NOT NULL,
 	id_libro             INTEGER NOT NULL,
-	PRIMARY KEY (id_autor,id_libro),
+	id_genero            INTEGER NOT NULL,
+	PRIMARY KEY (id_autor,id_libro,id_genero),
 	FOREIGN KEY R_1 (id_autor) REFERENCES autor (id_autor),
-	FOREIGN KEY R_3 (id_libro) REFERENCES libro (id_libro)
+	FOREIGN KEY R_3 (id_libro, id_genero) REFERENCES libro (id_libro, id_genero)
 );
 
 CREATE UNIQUE INDEX XPKautor_libro ON autor_libro
 (
 	id_autor,
-	id_libro
+	id_libro,
+	id_genero
 );
 
 CREATE INDEX XIF1autor_libro ON autor_libro
@@ -57,12 +76,13 @@ CREATE INDEX XIF1autor_libro ON autor_libro
 
 CREATE INDEX XIF2autor_libro ON autor_libro
 (
-	id_libro
+	id_libro,
+	id_genero
 );
 
 CREATE TABLE usuario
 (
-	id_usuario           INTEGER NOT NULL,
+	id_usuario           INTEGER AUTO_INCREMENT,
 	correo               VARCHAR(30) NOT NULL,
 	nombre_usuario       VARCHAR(30) NOT NULL,
 	contrasenia          VARCHAR(64) NOT NULL,
