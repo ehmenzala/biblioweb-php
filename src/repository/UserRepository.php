829d2d725dfdfe9825a0bf75c279b23074774a3f
ehmenzala <ehmenzala@gmail.com>
2023-06-15 10:40:56 -0500

    Add User service and repository

---
+++ src/repository/UserRepository.php
@@ -0,0 +1,9 @@
+<?php
+
+interface UserRepository {
+
+  public function get_by_username_and_passwd($username, $passwd);
+
+  public function add($user);
+
+}
---
+++ src/services/UserService.php
@@ -0,0 +1,48 @@
+<?php
+
+require BASE_PATH . '/src/repository/UserRepository.php';
+
+class UserService extends MainService implements UserRepository {
+
+  function __construct($db) {
+    self::init($db);
+  }
+
+  public function get_by_username_and_passwd($username, $passwd) {
+    $st = self::$db->prepare("
+      SELECT id_usuario, correo, nombre_usuario, contrasenia, rol
+      FROM usuario
+      WHERE nombre_usuario = :username AND contrasenia = :passwd
+    ");
+
+    $st->execute(array(
+      'username' => $username,
+      'passwd' => $passwd,
+    ));
+
+    $result = $st->fetch();
+
+    $user = new User(
+      $result['id_usuario'], $result['correo'],
+      $result['nombre_usuario'], $result['contrasenia'],
+      $result['rol']
+    );
+    
+    return $user;
+  }
+
+  public function add($user) {
+    $statement = self::$db->prepare("
+      INSERT INTO usuario(correo, nombre_usuario, contrasenia, rol)
+      VALUES (:email, :username, :passwd, :role)
+    ");
+
+    $statement->execute(array(
+      'email' => $user->get_email(),
+      'username' => $user->get_username(),
+      'passwd' => $user->get_passwd(),
+      'role' => $user->get_role())
+    );
+  }
+
+}
