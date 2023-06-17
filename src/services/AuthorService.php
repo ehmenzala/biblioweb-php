9a01279fab6460b817f343a2fede3b9faa680d8e
ehmenzala <ehmenzala@gmail.com>
2023-06-16 19:23:35 -0500

    Add Author Service

---
+++ src/services/AuthorService.php
@@ -0,0 +1,27 @@
+<?php
+
+require BASE_PATH . '/src/repository/AuthorRepository.php';
+
+class AuthorService extends MainService implements AuthorRepository {
+
+  function __construct($db) {
+    self::init($db);
+  }
+
+  public function get_all() {
+    $st = self::$db->prepare("
+      SELECT * FROM autor
+    ");
+    $st->execute();
+    $results = $st->fetchAll();
+    $authors = array_map(function($result) {
+      return new Author(
+        $result['id_autor'],
+        $result['nombre_completo'],
+        $result['foto_perfil']
+      );
+    }, $results);
+    return $authors;
+  }
+
+}
