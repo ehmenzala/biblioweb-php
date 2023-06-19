96797a9c1399e001b9fb139c58d4ae0e102a664d
ehmenzala <ehmenzala@gmail.com>
2023-06-19 15:40:08 -0500

    Add dashboard view

---
+++ public/js/dashboard-charts.js
@@ -0,0 +1,24 @@
+const ctxs = document.querySelectorAll(".chart");
+
+ctxs.forEach((ctx) => {
+  new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+      datasets: [
+        {
+          label: "# of Votes",
+          data: [12, 19, 3, 5, 2, 3],
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
+});
