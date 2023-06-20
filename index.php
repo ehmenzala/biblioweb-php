c1c4fed58b054471ab1dba124c8b806d7cc6a0f6
ehmenzala <ehmenzala@gmail.com>
2023-06-19 19:40:34 -0500

    Add charts to dashboard

--- index.php
+++ index.php
@@ -5,26 +5,40 @@ session_start();
 const BASE_PATH = __DIR__;
 
 require BASE_PATH . '/Router.php';
+require BASE_PATH . '/src/api/ApiRouter.php';
 
 require BASE_PATH . '/src/utilities/Authenticator.php';
 
 require BASE_PATH . '/src/models/User.php';
 require BASE_PATH . '/src/models/Book.php';
 require BASE_PATH . '/src/models/Author.php';
+require BASE_PATH . '/src/models/Genre.php';
 
 require BASE_PATH . '/src/services/MainService.php';
 require BASE_PATH . '/src/services/UserService.php';
 require BASE_PATH . '/src/services/BookService.php';
 require BASE_PATH . '/src/services/AuthorService.php';
 
-$router = new Router();
 $uri = $_SERVER['REQUEST_URI'];
 $method = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
+
+$db = new PDO('mysql:host=localhost;dbname=biblioweb', 'root');
+$userService = new UserService($db);
+$bookService = new BookService($db);
+
+$apiRouter = new ApiRouter($bookService, $userService);
+
+$apiRouter->handleRequest($uri, $method);
+
+$router = new Router();
 
 $router->get('/biblioweb/', 'src/views/index.html');
 $router->get('/biblioweb/libros/', 'src/views/libros.html');
 $router->get('/biblioweb/autores/', 'src/views/autores.html');
 $router->get('/biblioweb/rating/', 'src/views/rating.html');
 $router->get('/biblioweb/admin/', 'src/views/admin-view.html');
+$router->get('/biblioweb/dashboard/', 'src/views/dashboard.html');
+$router->get('/biblioweb/dashboard/bibliografia/', 'src/views/dashboard-bibliografia.html');
+$router->get('/biblioweb/dashboard/usuarios/', 'src/views/dashboard-usuarios.html');
 
 $router->route($uri, $method);
--- public/js/dashboard-charts.js
+++ public/js/dashboard-charts.js
@@ -1,24 +1,164 @@
-const ctxs = document.querySelectorAll(".chart");
+const $userCount = document.getElementById("user-count");
+const $bookCount = document.getElementById("book-count");
 
-ctxs.forEach((ctx) => {
-  new Chart(ctx, {
-    type: "bar",
-    data: {
-      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-      datasets: [
-        {
-          label: "# of Votes",
-          data: [12, 19, 3, 5, 2, 3],
-          borderWidth: 1,
+const $booksByGenreCtx = document.getElementById("books-by-genre");
+const $booksByRatingCtx = document.getElementById("books-by-rating");
+const $usersByRoleCtx = document.getElementById("users-by-role");
+
+const COLORS = [
+  "#A788E5",
+  "#DBD2EC",
+  "#A989E8",
+  "#615D69",
+  "#846BB5",
+  "#4C3E69",
+  "#A8A1B5",
+  "#846BB5",
+];
+
+setupUserCount();
+setupBookCount();
+setupUsersByRoleChart();
+setupBooksByGenreChart();
+setupBookByRatingChart();
+
+function setupUserCount() {
+  fetch("/biblioweb/usuarios/all/")
+    .then((r) => r.json())
+    .then((json) => {
+      const TOTAL_USERS = json.length;
+      $userCount.textContent = TOTAL_USERS;
+    });
+}
+
+function setupBookCount() {
+  fetch("/biblioweb/libros/all/")
+    .then((r) => r.json())
+    .then((json) => {
+      let TOTAL_BOOKS = json.length;
+      $bookCount.textContent = TOTAL_BOOKS;
+    });
+}
+
+function setupUsersByRoleChart() {
+  fetch("/biblioweb/usuarios/all/")
+    .then((r) => r.json())
+    .then((json) => {
+      let usersArr = json.map((user) => user.role);
+      let userRoleCount = usersArr.reduce((acc, curr) => {
+        if (acc[curr]) {
+          acc[curr]++;
+        } else {
+          acc[curr] = 1;
+        }
+        return acc;
+      }, {});
+
+      const data = {
+        labels: [...Object.keys(userRoleCount)],
+        datasets: [
+          {
+            label: "Rol",
+            data: [...Object.values(userRoleCount)],
+            backgroundColor: [...COLORS],
+          },
+        ],
+      };
+
+      new Chart($usersByRoleCtx, {
+        type: "pie",
+        data,
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true,
+            },
+          },
         },
-      ],
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true,
+      });
+    });
+}
+
+function setupBooksByGenreChart() {
+  fetch("/biblioweb/libros/all/")
+    .then((r) => r.json())
+    .then((json) => {
+      let genresArr = json.map((book) => book.genre);
+      let genresCount = genresArr.reduce((acc, curr) => {
+        if (acc[curr]) {
+          acc[curr]++;
+        } else {
+          acc[curr] = 1;
+        }
+        return acc;
+      }, {});
+
+      const data = {
+        labels: [...Object.keys(genresCount)],
+        datasets: [
+          {
+            label: "Género",
+            data: [...Object.values(genresCount)],
+            backgroundColor: [...COLORS],
+          },
+        ],
+      };
+
+      new Chart($booksByGenreCtx, {
+        type: "pie",
+        data,
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true,
+            },
+          },
         },
-      },
-    },
-  });
-});
+      });
+    });
+}
+
+function setupBookByRatingChart() {
+  fetch("/biblioweb/libros/all/")
+    .then((r) => r.json())
+    .then((json) => {
+      let ratingArr = json.map((book) => book.rating);
+      let ratingCount = ratingArr.reduce((acc, curr) => {
+        if (acc[curr]) {
+          acc[curr]++;
+        } else {
+          acc[curr] = 1;
+        }
+        return acc;
+      }, {});
+
+      const data = {
+        labels: [...Object.keys(ratingCount)],
+        datasets: [
+          {
+            label: "Rating",
+            data: [...Object.values(ratingCount)],
+            backgroundColor: [...COLORS],
+          },
+        ],
+      };
+
+      new Chart($booksByRatingCtx, {
+        type: "bar",
+        data,
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true,
+            },
+          },
+        },
+      });
+    });
+}
+
+async function fetchBookByGenreId(genreId) {
+  const response = await fetch(`/biblioweb/libros/genero/${genreId}/`);
+  const books = await response.json();
+  console.log(books);
+}
