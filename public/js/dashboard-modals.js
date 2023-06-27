64e19d3829dafb29c65b8af1d2312ccaf9818c80
ehmenzala <ehmenzala@gmail.com>
2023-06-27 17:03:58 -0500

    Add user action dialog

--- public/js/dashboard-modals.js
+++ public/js/dashboard-modals.js
@@ -2,79 +2,103 @@ const $openBooksDialog = document.getElementById("open-books-crud");
 const $openUsersDialog = document.getElementById("open-users-crud");
 const $openGenreDialog = document.getElementById("open-genre-dialog");
 const $openAuthorDialog = document.getElementById("open-author-dialog");
-const $openCreateUserDialog = document.getElementById(
-  "open-create-user-dialog"
+const $openUserActionDialog = document.getElementById(
+  "open-user-action-dialog"
 );
 
 const $closeBooksDialog = document.getElementById("close-books-dialog");
 const $closeUsersDialog = document.getElementById("close-users-dialog");
 const $closeGenreDialog = document.getElementById("close-genre-dialog");
 const $closeAuthorDialog = document.getElementById("close-author-dialog");
-const $closeCreateUserDialog = document.getElementById(
-  "close-create-user-dialog"
-);
+const $closeUserAction = document.getElementById("close-user-action-dialog");
 
 const $booksDialog = document.getElementById("books-dialog");
 const $usersDialog = document.getElementById("users-dialog");
 const $genreDialog = document.getElementById("genre-dialog");
 const $authorDialog = document.getElementById("author-dialog");
-const $createUserDialog = document.getElementById("create-user-dialog");
+const $userActionDialog = document.getElementById("user-action-dialog");
 
 const $profileAuthorInput = document.getElementById("profile-img");
 const $authorInput = document.getElementById("new-author");
+const $userActionInput = document.getElementById("user-action-input");
 
 const $bookFormStatus = document.getElementById("book-status"); // Currently unused
+
+/* Dynamic dialogs behavior */
+
+/* *** Manage books */
 
 $authorInput.addEventListener("input", (e) => {
   const authorName = e.target.value.toLowerCase().replace(/\s/g, "");
 
-  if (authorName === "") {
-    $profileAuthorInput.value = "";
-  } else {
-    $profileAuthorInput.value = `https://api.dicebear.com/6.x/lorelei/svg?seed=${authorName}`;
-  }
+  $profileAuthorInput.value =
+    authorName === ""
+      ? ""
+      : `https://api.dicebear.com/6.x/lorelei/svg?seed=${authorName}`;
 });
 
-/* OPEN DIALOGS */
+/* *** User action */
 
-$openBooksDialog.addEventListener("click", () => {
-  $booksDialog.showModal();
+const $userActionForm = document.getElementById("user-action-form");
+const $userActionTitle = document.getElementById("user-action-title");
+const $userActionUserId = document.getElementById("user-action-title-id");
+const $userActionText = document.getElementById("user-action-text");
+
+$openUserActionDialog.addEventListener("click", () => {
+  $userActionForm.reset();
+  $userActionTitle.textContent = "Creando usuario...";
+  $userActionText.textContent = "Estás creando un nuevo usuario.";
+  $userActionUserId.textContent = "";
+
+  // $userActionTitle.textContent = "Modificando usuario";
+  // $userActionText.textContent = "Estás modificando un usuario existente.";
+  // $userActionUserId.textContent = "- ID 5";
 });
 
-$openUsersDialog.addEventListener("click", () => {
-  $usersDialog.showModal();
-});
+/* Dialogs */
 
-$openGenreDialog.addEventListener("click", () => {
-  $genreDialog.showModal();
-});
+setupDialogs();
 
-$openAuthorDialog.addEventListener("click", () => {
-  $authorDialog.showModal();
-});
+function setupDialogs() {
+  $openBooksDialog.addEventListener("click", () => {
+    $booksDialog.showModal();
+  });
 
-$openCreateUserDialog.addEventListener("click", () => {
-  $createUserDialog.showModal();
-});
+  $openUsersDialog.addEventListener("click", () => {
+    $usersDialog.showModal();
+  });
 
-/* CLOSE DIALOGS */
+  $openGenreDialog.addEventListener("click", () => {
+    $genreDialog.showModal();
+  });
 
-$closeBooksDialog.addEventListener("click", () => {
-  $booksDialog.close();
-});
+  $openAuthorDialog.addEventListener("click", () => {
+    $authorDialog.showModal();
+  });
 
-$closeUsersDialog.addEventListener("click", () => {
-  $usersDialog.close();
-});
+  $openUserActionDialog.addEventListener("click", () => {
+    $userActionDialog.showModal();
+  });
 
-$closeGenreDialog.addEventListener("click", () => {
-  $genreDialog.close();
-});
+  /* Close dialogs */
 
-$closeAuthorDialog.addEventListener("click", () => {
-  $authorDialog.close();
-});
+  $closeBooksDialog.addEventListener("click", () => {
+    $booksDialog.close();
+  });
 
-$closeCreateUserDialog.addEventListener("click", () => {
-  $createUserDialog.close();
-});
+  $closeUsersDialog.addEventListener("click", () => {
+    $usersDialog.close();
+  });
+
+  $closeGenreDialog.addEventListener("click", () => {
+    $genreDialog.close();
+  });
+
+  $closeAuthorDialog.addEventListener("click", () => {
+    $authorDialog.close();
+  });
+
+  $closeUserAction.addEventListener("click", () => {
+    $userActionDialog.close();
+  });
+}
